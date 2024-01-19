6efd840c380957d343b25feade8debd26772dd9b
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-20 00:29:51 +0900

    feat: S2-L4まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,87 @@ import {
   useContract,
 } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const address = useAddress();
+  console.log("👋Wallet Address: ", address);
+
   const chain = useChain();
 
+  // editionDrop コントラクトを初期化
+  const editionDrop = useContract(
+    "0xFA8e64e19cc3055f816b11265e93F037732c8e58",
+    "edition-drop"
+  ).contract;
+
+  // ユーザーがメンバーシップ NFT を持っているかどうかを知るためのステートを定義
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  // NFT をミンティングしている間を表すステートを定義
+  const [isClaiming, setIsClaiming] = useState(false);
+
+  useEffect(() => {
+    // もしウォレットに接続されていなかったら処理をしない
+    if (!address) {
+      return;
+    }
+    // ユーザーがメンバーシップ NFT を持っているかどうかを確認する関数を定義
+    const checkBalance = async () => {
+      try {
+        const balance = await editionDrop!.balanceOf(address, 0);
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 this user has a membership NFT!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 this user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Failed to get balance", error);
+      }
+    };
+    // 関数を実行
+    checkBalance();
+  }, [address, editionDrop]);
+
+  const mintNft = async () => {
+    try {
+      setIsClaiming(true);
+      await editionDrop!.claim("0", 1);
+      console.log(
+        `🌊 Successfully Minted! Check it out on etherscan: https://sepolia.etherscan.io/address/${editionDrop!.getAddress()}`
+      );
+      setHasClaimedNFT(true);
+    } catch (error) {
+      setHasClaimedNFT(false);
+      console.error("Failed to mint NFT", error);
+    } finally {
+      setIsClaiming(false);
+    }
+  };
+
+  // ウォレットと接続していなかったら接続を促す
+  if (!address) {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome to Tokyo Sauna Collective !!</h1>
+          <div className={styles.connect}>
+            <ConnectWallet />
+          </div>
+        </main>
+      </div>
+    );
+  }
   // テストネットが Sepolia ではなかった場合に警告を表示
-  if (chain && chain.chainId !== Sepolia.chainId) {
+  else if (chain && chain.chainId !== Sepolia.chainId) {
     console.log("wallet address: ", address);
     console.log("chain name: ", chain.name);
+
     return (
       <div className={styles.container}>
         <main className={styles.main}>
@@ -26,14 +96,27 @@ const Home: NextPage = () => {
         </main>
       </div>
     );
-  } else {
+  }
+  // DAO ダッシュボード画面を表示
+  else if (hasClaimedNFT) {
     return (
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>Welcome to micoy Fashion DAO !!</h1>
-          <div className={styles.connect}>
-            <ConnectWallet />
-          </div>
+          <h1 className={styles.title}>🍪DAO Member Page</h1>
+          <p>Congratulations on being a member</p>
+        </main>
+      </div>
+    );
+  }
+  // ウォレットと接続されていたら Mint ボタンを表示
+  else {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Mint your free 🍪DAO Membership NFT</h1>
+          <button disabled={isClaiming} onClick={mintNft}>
+            {isClaiming ? "Minting..." : "Mint your nft (FREE)"}
+          </button>
         </main>
       </div>
     );
