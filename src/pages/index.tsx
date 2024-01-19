fb21089a24f37594392b6114757ed524fa9ceb3d
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-20 02:01:59 +0900

    feat: S3-L2まで

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import {
   useContract,
 } from "@thirdweb-dev/react";
 import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import styles from "../styles/Home.module.css";
 
@@ -22,11 +22,85 @@ const Home: NextPage = () => {
     "edition-drop"
   ).contract;
 
+  // トークンコントラクトの初期化
+  const token = useContract(
+    "0x2e9E3B9336Db994A23AFf7DF358Be6E6BbeFDE0c",
+    "token"
+  ).contract;
+
   // ユーザーがメンバーシップ NFT を持っているかどうかを知るためのステートを定義
   const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
 
   // NFT をミンティングしている間を表すステートを定義
   const [isClaiming, setIsClaiming] = useState(false);
+
+  // メンバーごとの保有しているトークンの数をステートとして宣言
+  const [memberTokenAmounts, setMemberTokenAmounts] = useState<any>([]);
+
+  // DAO メンバーのアドレスをステートで宣言
+  const [memberAddresses, setMemberAddresses] = useState<string[] | undefined>(
+    []
+  );
+
+  // アドレスの長さを省略してくれる便利な関数
+  const shortenAddress = (str: string) => {
+    return str.substring(0, 6) + "..." + str.substring(str.length - 4);
+  };
+
+  // メンバーシップを保持しているメンバーの全アドレスを取得します
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    // 先ほどエアドロップしたユーザーがここで取得できます（発行された tokenID 0 のメンバーシップ NFT）
+    const getAllAddresses = async () => {
+      try {
+        const memberAddresses =
+          await editionDrop?.history.getAllClaimerAddresses(0);
+        setMemberAddresses(memberAddresses);
+        console.log("🚀 Members addresses", memberAddresses);
+      } catch (error) {
+        console.error("failed to get member list", error);
+      }
+    };
+    getAllAddresses();
+  }, [hasClaimedNFT, editionDrop?.history]);
+
+  // 各メンバーが保持するトークンの数を取得します
+  useEffect(() => {
+    if (!hasClaimedNFT) {
+      return;
+    }
+
+    const getAllBalances = async () => {
+      try {
+        const amounts = await token?.history.getAllHolderBalances();
+        setMemberTokenAmounts(amounts);
+        console.log("👜 Amounts", token);
+      } catch (error) {
+        console.error("failed to get member balances", error);
+      }
+    };
+    getAllBalances();
+  }, [hasClaimedNFT, token?.history]);
+
+  // memberAddresses と memberTokenAmounts を 1 つの配列に結合します
+  const memberList = useMemo(() => {
+    return memberAddresses?.map((address) => {
+      // memberTokenAmounts 配列でアドレスが見つかっているかどうかを確認します
+      // その場合、ユーザーが持っているトークンの量を返します
+      // それ以外の場合は 0 を返します
+      const member = memberTokenAmounts?.find(
+        ({ holder }: { holder: string }) => holder === address
+      );
+
+      return {
+        address,
+        tokenAmount: member?.balance.displayValue || "0",
+      };
+    });
+  }, [memberAddresses, memberTokenAmounts]);
 
   useEffect(() => {
     // もしウォレットに接続されていなかったら処理をしない
@@ -98,12 +172,37 @@ const Home: NextPage = () => {
     );
   }
   // DAO ダッシュボード画面を表示
+  // ユーザーがすでに NFT を要求している場合は、内部 DAO ページを表示します
+  // これは DAO メンバーだけが見ることができ、すべてのメンバーとすべてのトークン量をレンダリングします
   else if (hasClaimedNFT) {
     return (
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>🍪DAO Member Page</h1>
           <p>Congratulations on being a member</p>
+          <div>
+            <div>
+              <h2>Member List</h2>
+              <table className="card">
+                <thead>
+                  <tr>
+                    <th>Address</th>
+                    <th>Token Amount</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {memberList!.map((member) => {
+                    return (
+                      <tr key={member.address}>
+                        <td>{shortenAddress(member.address)}</td>
+                        <td>{member.tokenAmount}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </main>
       </div>
     );
