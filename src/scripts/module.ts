aa023ef2814d422c340c582c10ad10dfa6321b44
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-20 12:26:08 +0900

    feat: S3-L3まで

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,4 +1,4 @@
 export const editionDropAddress = "0xFA8e64e19cc3055f816b11265e93F037732c8e58";
 export const ERCTokenAddress = "0x2e9E3B9336Db994A23AFf7DF358Be6E6BbeFDE0c";
-export const governanceAddress = "";
+export const governanceAddress = "0xA84DE7141f394Aa1C909eD6472Da05022E1c9767";
 export const ownerWalletAddress = "0xF617B53b3c9b4523e571f23d869aCD115D2F84c5";
