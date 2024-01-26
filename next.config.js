89be77bac08dad7f91a8b06adbdc0a70a2deffa7
lycp152 <118104209+lycp152@users.noreply.github.com>
2024-01-26 17:52:09 +0900

    fix: デプロイエラーを修正

--- next.config.js
+++ next.config.js
@@ -3,4 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = module.exports = {
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  nextConfig,
+};
